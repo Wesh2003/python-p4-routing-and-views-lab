83626747b5684fb22562be67a05554f76945ebf0
Tumaini Muriithi <wendomuriithi@gmail.com>
2024-01-23 22:22:55 +0300

    Almost done

--- server/app.py
+++ server/app.py
@@ -4,5 +4,15 @@ from flask import Flask
 
 app = Flask(__name__)
 
+
+@app.route('/')
+def user():
+    return f'<h1>Python Operations with Flask Routing and Views</h1>'
+
+@app.route('/print/<hello>')
+def user(hello):
+    return f'<h1>Profile for {hello}</h1>'
+
+
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
